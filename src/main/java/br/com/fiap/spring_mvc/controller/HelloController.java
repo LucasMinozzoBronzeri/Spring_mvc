d7a64fb82446c449f003ef930f3e7d99137791f0
Lucas <lucasminozzobronzeri@gmail.com>
2025-02-14 09:51:02 -0300

    Fazendo HTML do projeto em classe

--- src/main/java/br/com/fiap/spring_mvc/controller/HelloController.java
+++ src/main/java/br/com/fiap/spring_mvc/controller/HelloController.java
@@ -1,2 +1,35 @@
-package br.com.fiap.spring_mvc.controler;public class HelloController {
+package br.com.fiap.spring_mvc.controller;
+
+import br.com.fiap.spring_mvc.model.Categoria;
+import br.com.fiap.spring_mvc.model.Livro;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloController {
+    @GetMapping("/ola")
+    public String hello(Model model){
+        model.addAttribute("message", "Hello world!");
+        return "hello"; //Nome do template HTML, ex: "hello.html"
+    }
+
+    @GetMapping("/ola2")
+    public ModelAndView hello(){
+        ModelAndView mv = new ModelAndView("hello");
+        mv.addObject("message", "Helooooooo");
+        return mv;
+    }
+
+    @GetMapping("/livro")
+    public ModelAndView livro(){
+        Livro livro = new Livro();
+        livro.setAutor("Homero");
+        livro.setTitulo("Odisseia");
+        livro.setCategoria(Categoria.ROMANCE);
+        ModelAndView mv = new ModelAndView("livro");
+        mv.addObject("livro", livro);
+        return mv;
+    }
 }
